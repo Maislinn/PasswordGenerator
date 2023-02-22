d3cce39a8498d2905313c7e102a18826ea3133c2
Maislinn <maislinn.helfer@gmail.com>
2023-02-21 19:01:08 -0800

    Debuging

--- Develop/script.js
+++ Develop/script.js
@@ -12,29 +12,32 @@ var specialChars = "!@#$%^&*()_+=-.,/~";
 
 //function to generate password & empty stings to store user input
 function generatePassword() {
-    var allChars = "";
+    var allChar = "";
     var userPassword = "";
 
 
 //function to prompt the user for password criteria
 
-function promptUser() {
-    var passwordLength = prompt("How many characters would you like your password to contain?");
+//function promptUser() {
+var passwordLength = parseInt(prompt("How many characters would you like your password to contain?"));
     if (passwordLength < 10 || passwordLength > 120) { 
         alert("Password length must be between 10 and 120 characters. Please try again.");
-        return;
-    } else if (passwordLength < 10) {
+        generatePassword();
+    } ;
+    if (passwordLength < 10) {
         alert("Password length must be at least 10 characters. Please try again.");
         return;
-    } else if (passwordLength > 120) {
+    } ;
+    if (passwordLength > 120) {
         alert("Password length must be less than 120 characters. Please try again.");
         return;
-    } else {
+    } 
+    else {
         var upAlpha = confirm("Click OK to confirm including uppercase letters.");
         var lowercase = confirm("Click OK to confirm including lowercase letters.");
         var numbers = confirm("Click OK to confirm including numbers.");
         var specialChars = confirm("Click OK to confirm including special characters.");
-    };
+    //};
     
 // if user does not select any character types, alert user
     if (upAlpha === false && lowercase === false && numbers === false && specialChars === false) { 
@@ -63,8 +66,9 @@ function promptUser() {
     };
 
     // for loop to generate password randomly
-    for (var i = userPassword.length; i < passwordLength; i++) {
-        userPassword += allChars[Math.floor(Math.random() * allChars.length)];
+    for (var i = 0; i < truePasswordLength; i++) {
+        var randomChar = allChar[Math.floor(Math.random() * allChar.length)];
+        userPassword += allChar[randomChar];
         console.log(userPassword);
     };
     return userPassword;
